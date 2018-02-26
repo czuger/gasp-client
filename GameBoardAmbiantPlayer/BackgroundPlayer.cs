cee26a2d5f65ba78e9bb91dc825f1782e0c41218
Cédric Zuger <zuger.cedric@gmail.com>
2018-02-26 15:37:34 +0100

    Background Player working.

---
+++ GameBoardAmbiantPlayer/BackgroundPlayer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.IO;
+
+public class BackgroundPlayer
+{
+    WaveOutEvent outputDevice = null;
+    AudioFileReader audioFile = null;
+    FadeInOutSampleProvider fader = null;
+    public bool interrupt_play = false;
+    private bool playback_cleanup_finished = false;
+    Array files_list = null;
+    string path = null;
+    int fade_in_time = 5000;
+    int fade_out_time = 2000;
+
+    public BackgroundPlayer( string _path, Array _files_list )
+	{
+        // Déclaration du thread
+        Thread myThread;
+        files_list = _files_list;
+        path = _path;
+
+        // Instanciation du thread, on spécifie dans le 
+        // délégué ThreadStart le nom de la méthode qui
+        // sera exécutée lorsque l'on appele la méthode
+        // Start() de notre thread.
+        myThread = new Thread(new ThreadStart(ThreadLoop));
+
+        // Lancement du thread
+        myThread.Start();
+    }
+
+    public void play_music()
+    {
+        foreach (string fileName in files_list)
+        {
+            audioFile = new AudioFileReader(@path + fileName);
+            fader = new FadeInOutSampleProvider(audioFile, true);
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+            outputDevice.Init(fader);
+            fader.BeginFadeIn(fade_in_time);
+            outputDevice.Play();
+            while (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(500);
+                if (interrupt_play)
+                {
+                    fader.BeginFadeOut(fade_out_time);
+                    Thread.Sleep(fade_out_time);
+                    outputDevice.Stop();
+                    return;
+                }
+            }
+        }
+    }
+
+    // C'est ici qu'il faudra faire notre travail.
+    public void ThreadLoop()
+    {
+        // Tant que le thread n'est pas tué, on travaille
+        while (Thread.CurrentThread.IsAlive && !interrupt_play)
+        {
+            play_music();
+        }
+
+        while (outputDevice.PlaybackState == PlaybackState.Playing && !playback_cleanup_finished)
+        {
+            Thread.Sleep(500);
+        }
+    }
+
+    private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+    {
+        outputDevice.Dispose();
+        outputDevice = null;
+        audioFile.Dispose();
+        audioFile = null;
+        playback_cleanup_finished = true;
+    }
+
+}
--- GameBoardAmbiantPlayer/Form1.cs
+++ GameBoardAmbiantPlayer/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
 using NAudio.Wave;
@@ -17,38 +18,49 @@ namespace GameBoardAmbiantPlayer
 
     public partial class Form1 : Form
     {
-        private WaveOutEvent outputDevice1, outputDevice2;
-        private FadeInOutSampleProvider fade1, fade2;
-        private AudioFileReader audioFile1, audioFile2;
-        private string path1 = "\\\\DISKSTATION\\vol1\\Medias\\Wow music\\Base\\Sound\\Music\\ZoneMusic\\Forest\\";
-        private string path2 = "\\\\DISKSTATION\\vol1\\Medias\\Wow music\\Base\\Sound\\Music\\CityMusic\\Orgrimmar\\";
+        private static string forest = "../../sounds/Forest/";
+        private static string battle = "../../sounds/battle/";
+        BackgroundPlayer player_thread = null;
+
+        private void Battle_Click(object sender, EventArgs e)
+        {
+            player_thread.interrupt_play = true;
+            var files = Directory.GetFiles(battle);
+            player_thread = new BackgroundPlayer(battle, files);
+        }
 
         public Form1()
         {
             InitializeComponent();
+            var files = Directory.GetFiles(forest);
+            player_thread = new BackgroundPlayer(forest, files);
+
+            //audioFile1 = new AudioFileReader(@path1 + "DayForest02.mp3");
+            //audioFile2 = new AudioFileReader(@path2 + "orgrimmar02-moment.mp3");
+            //fade1 = new FadeInOutSampleProvider(audioFile1, true);
+            //fade2 = new FadeInOutSampleProvider(audioFile2, true);
+            //outputDevice1 = new WaveOutEvent();
+            //outputDevice1.Init(fade1);
+            //outputDevice2 = new WaveOutEvent();
+            //outputDevice2.Init(fade2);
         }
+
+
 
         private void button2_Click(object sender, EventArgs e)
         {
             // Thread.Sleep(10000);
-            fade1.BeginFadeOut(3000);
-            Thread.Sleep(2000);
-            outputDevice2.Play();
-            fade2.BeginFadeIn(3000);
+            //fade1.BeginFadeOut(3000);
+            //Thread.Sleep(2000);
+            //outputDevice2.Play();
+            //fade2.BeginFadeIn(3000);
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {            
-            audioFile1 = new AudioFileReader(@path1 + "DayForest02.mp3");
-            audioFile2 = new AudioFileReader(@path2 + "orgrimmar02-moment.mp3");
-            fade1 = new FadeInOutSampleProvider(audioFile1, true);
-            fade2 = new FadeInOutSampleProvider(audioFile2, true);
-            outputDevice1 = new WaveOutEvent();
-            outputDevice1.Init(fade1);
-            outputDevice1.Play();
-            outputDevice2 = new WaveOutEvent();
-            outputDevice2.Init(fade2);
-            fade1.BeginFadeIn(1000);
+        {
+            //play_forest();
+            //outputDevice1.Play();
+            //fade1.BeginFadeIn(1000);
             // outputDevice1.Play();
         }
     }
